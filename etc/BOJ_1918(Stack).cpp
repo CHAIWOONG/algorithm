639d6683d02f967acd4b45e32f9fdeb802d4b75d
CHAIWOONG <80874836+CHAIWOONG@users.noreply.github.com>
2022-01-22 19:00:18 +0900

    Update BOJ_1918(Stack).cpp

--- etc/BOJ_1918(Stack).cpp
+++ etc/BOJ_1918(Stack).cpp
@@ -11,6 +11,7 @@ using namespace std;
 
 int conv[128];
 
+// 연산자 입력에 대해서 우선 순위의 파악 + 시간 복잡도의 간소화를 위해서 배열을 통해 숫자로 판단
 void convert() {
 	conv['('] = conv[')'] = 1;
 	conv['+'] = conv['-'] = 2;
@@ -27,17 +28,17 @@ int main(){
     stack<char> stk;
     
     cin>>str;
-    convert();
+    convert(); // 연산자 우선순위 파악을 위한 배열 생성
     
-    for(int i=0; i<str.length(); i++){
+    for(int i=0; i<str.length(); i++){ // string의 반복은 str.length 만큼으로만 지정하여 time dumped 오류를 예방하자
         
-        if(str[i]>='A'&& str[i]<='Z') {
+        if(str[i]>='A'&& str[i]<='Z') {  // 숫자는 stack에 저장하지 않고 그대로 출력
             cout<<str[i];
         }
-        else if(str[i]=='('){
+        else if(str[i]=='('){  // 왼쪽 괄호는 무조건 stack에 포함
             stk.push(str[i]);
         }
-        else if(str[i]==')'){
+        else if(str[i]==')'){ // 오른쪽 괄호는 왼쪽 괄호가 나타나기 전까지 안에있는 것을 모두 pop
             
             while(!stk.empty()&&stk.top()!='(') {
                 
@@ -46,7 +47,7 @@ int main(){
             }
             stk.pop();
         }
-        else{
+        else{ // 괄호가 아닌 연산자에 대해서는 우선순위를 판단하여 stack에 push할지 pop 할지 판단한다
    
             while(!stk.empty()&&conv[str[i]]<=conv[stk.top()]) {
                 
@@ -57,7 +58,7 @@ int main(){
         }
     }
     
-    while(!stk.empty()) {
+    while(!stk.empty()) { // stack에 남아있는 연산자를 전부 출력
         cout<<stk.top();
         stk.pop();
     }
