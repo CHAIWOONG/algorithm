86cbd25e5d4e25aab2de2878547fe0c49fca1c71
CHAIWOONG <80874836+CHAIWOONG@users.noreply.github.com>
2022-02-01 07:28:57 +0900

    Update BOJ_1781(Priority_queue)_____.cpp

--- Data structure/BOJ_1781(Priority_queue)_____.cpp
+++ Data structure/BOJ_1781(Priority_queue)_____.cpp
@@ -5,12 +5,16 @@
 
 using namespace std;
 
-//priority_queue<pair<int,int>> mxhp; // 컵라면수, 데드라인의 max heap
-//priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> mnhp;
-// 데드라인과 컵라면 수에 대한 min Heap
-// ***** heap을 여러개 만들어서 이를 삭제하는 과정을 반복하게 되면 시간복잡도가 증가할 것이다
+// ----------------min heap의 사이즈 조절을 통해 메모리를 효율적으로 사용하여 n번째의 최대값 구하기 (2075와 비슷) 
 
+// --- min_heap으로 선언하여 가장 큰 N개의 수만 가지고서 pq에 넣으면서 크기가 N보다 크면 가장 작은 것 하나 버리는 것을 반복하면 끝날때는 N개가 남을 것이고 그 중 가장 작은 것을 출력
 priority_queue<int, vector<int>, greater<int>> mnhp; // 컵라면 수에 대한 min heap
+
+
+//priority_queue<pair<int,int>> mxhp; // (컵라면수, 데드라인)의 max heap
+//priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> mnhp;
+// (데드라인과 컵라면) 수에 대한 min Heap
+// ***** 이렇게 heap을 여러개 만들어서 이를 삭제하는 과정을 반복하게 되면 시간복잡도가 증가할 것이다
 
 int main()
 {
