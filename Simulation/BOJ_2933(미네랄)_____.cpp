a5910afce041a51d485e13cb80b08ca46439ca2c
CHAIWOONG <80874836+CHAIWOONG@users.noreply.github.com>
2022-02-15 22:10:20 +0900

    Update and rename BOJ_2933(미네랄).cpp to BOJ_2933(미네랄)_____.cpp

--- Simulation/BOJ_2933(미네랄)_____.cpp
+++ Simulation/BOJ_2933(미네랄)_____.cpp
@@ -16,7 +16,7 @@ int dy[4] = { 0,1,0,-1 };
 vector<int> v; // 막대기 던지는 row, (row-i)로 저장되어있음
 vector<pii> clusterV;
 
-void stkThr(int flag) { // vector에 저장한 막대기 높이 받아와 던짐
+void stkThr(int flag) { // ------------------------------------------------------vector에 저장한 막대기 높이 받아와 던짐
 
     if (flag % 2 == 0) { // 왼쪽에서 던짐
 
@@ -36,7 +36,7 @@ void stkThr(int flag) { // vector에 저장한 막대기 높이 받아와 던짐
     }
 }
 
-void clusterDown() { // 클러스터에 대해서 뭉쳐서 중력으로 내려옴
+void clusterDown() { // ------------------------------------------------------------- 같은 클러스터에서 원소가 같은 거리 만큼 뭉쳐서 중력으로 내려옴
 
     int dis = 100;
     int visflag = vis[clusterV[0].X][clusterV[0].Y];
@@ -47,7 +47,7 @@ void clusterDown() { // 클러스터에 대해서 뭉쳐서 중력으로 내려�
         int tmpY = clusterV[i].Y;
         int tmpdis;
 
-        if (tmpX == row - 1) { // 해당 클러스터가 바닥과 닿아있으면 내릴꺼 없이 함수 종료
+        if (tmpX == row - 1) { // ----------------------------------------해당 클러스터가 바닥과 닿아있으면 내릴꺼 없이 함수 종료
             clusterV.clear();
             return;
         }
@@ -56,19 +56,19 @@ void clusterDown() { // 클러스터에 대해서 뭉쳐서 중력으로 내려�
         int nrow = 1;
         while (1) {
 
-            if ((tmpX + nrow == row)) break; // 바닥에 닿으면 증가를 멈춤
+            if ((tmpX + nrow == row)) break; // ------------------------------------------------바닥에 닿으면 증가를 멈춤
             if ((board[tmpX + nrow][tmpY] == 'x' && vis[tmpX + nrow][tmpY] != visflag)) break;
-            // 미네랄에 닿았는데 그것이 vis로 보았을 때 다른 미네랄이라면 증가를 멈춤
+            // ---------------------------------------------------------------미네랄에 닿았는데 그것이 vis로 보았을 때 다른 미네랄이라면 증가를 멈춤
             nrow++;
         }
         
-        if (nrow - 1 < dis) dis = nrow - 1; // 최소값의 내림 거리 계산
+        if (nrow - 1 < dis) dis = nrow - 1; // ----------------------------------------------최소값의 내림 거리 계산
     }
     
     //******** 삭제와 추가를 동시에 진행하면 같은 클러스터에서 같은 열의 다른 행이 존재하는 경우 엉켜서 지워진다. 
     // 삭제 따로 마무리 하고 dis만큼 더하여 한번에 추가하는 방식으로 해야 한다.
     
-    for (int i = 0; i < clusterV.size(); i++) { // ******** 이동하기 전에 것은 삭제
+    for (int i = 0; i < clusterV.size(); i++) { // -----------------------------------******** 이동하기 전에 것은 삭제
 
         int tmpX = clusterV[i].X;
         int tmpY = clusterV[i].Y;
@@ -77,7 +77,7 @@ void clusterDown() { // 클러스터에 대해서 뭉쳐서 중력으로 내려�
         vis[tmpX][tmpY] = 0;
     }
     
-    for (int i = 0; i < clusterV.size(); i++) { // ********* dis 만큼 이동하여 추가
+    for (int i = 0; i < clusterV.size(); i++) { //------------------------------------ ********* dis 만큼 이동하여 추가
 
         int tmpX = clusterV[i].X;
         int tmpY = clusterV[i].Y;
@@ -90,7 +90,7 @@ void clusterDown() { // 클러스터에 대해서 뭉쳐서 중력으로 내려�
     return;
 }
 
-void clusterBfs(int& r, int& c) { // 출발점 받아서 bfs로 같은 클러스터 인지 확인
+void clusterBfs(int& r, int& c) { // ----------------------------------------------- 출발점 받아서 bfs로 같은 클러스터 인지 확인
 
     queue<pii> q; q.push({ r,c }); clusterV.push_back({ r,c });
     vis[r][c] = clustercnt;
@@ -115,7 +115,7 @@ void clusterBfs(int& r, int& c) { // 출발점 받아서 bfs로 같은 클러스
     clustercnt++; // 하나의 bfs에서 클러스터 구분을 위해 증가
 }
 
-void clusterCheck() {
+void clusterCheck() { // --------------------------------------------------- 아래에서부터 (제일 높은 행) bfs를 통해 같은 클러스터 인지 확인
 
     for (int i = row - 1; i >= 0; i--) for (int j = 0; j < col; j++) {
         if (board[i][j] == 'x' && !vis[i][j]) {
@@ -142,8 +142,8 @@ int main()
 
     for (int j = 0; j < v.size(); j++) {
 
-        stkThr(j); // 막대기 던짐
-        clusterCheck(); // 미네랄 떨어질꺼 떨어지고
+        stkThr(j); // ----------------------------------------막대기 던짐
+        clusterCheck(); // ----------------------------------미네랄 떨어질꺼 떨어지고
         
         for (int i = 0; i < row; i++) for(int j2 = 0; j2 < col; j2++) vis[i][j2] = 0; // vis 초기화
     }
