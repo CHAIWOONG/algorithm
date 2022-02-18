fcc8d81183c8a5d134f5c47c843b54149a3eb070
CHAIWOONG <80874836+CHAIWOONG@users.noreply.github.com>
2022-02-18 09:35:39 +0900

    Update BOJ_2206(BFS,벽부수기1)____.cpp

--- Graph/DFS&BFS/BOJ_2206(BFS,벽부수기1)____.cpp
+++ Graph/DFS&BFS/BOJ_2206(BFS,벽부수기1)____.cpp
@@ -7,12 +7,12 @@ using pii = pair<int, int>;
 
 int row, col, ocnt = 0;
 int board[1001][1001];
-int dis[1001][1001][2]; // *******벽을 뚫고 온 경로와 그렇지 않은 경로를 구분시켜 줘야 한다
+int dis[1001][1001][2]; // ******** 벽을 뚫고 온 경로와 그렇지 않은 경로를 구분시켜 줘야 한다 (상태가 분화되므로 이를 차원을 늘려서 구현)
 
 int dx[4] = { 1,0,-1,0 };
 int dy[4] = { 0,1,0,-1 };
 
-queue<pair<pii, int>> q;
+queue<pair<pii, int>> q;  // int block = 1이면 아직 벽돌은 꺤적이 없는 것이다
 
 /*void crash(int r, int c){
 
@@ -31,7 +31,7 @@ int bfs() {
         int block = q.front().second;
         q.pop();
 
-        if (temp.X == row - 1 && temp.Y == col - 1) {
+        if (temp.X == row - 1 && temp.Y == col - 1) { // 현재의 값이 목적지의 좌표이면 return
             return dis[temp.X][temp.Y][block];
         }
 
@@ -41,19 +41,16 @@ int bfs() {
             int ny = temp.Y + dy[i];
 
             if (nx < 0 || nx >= row || ny < 0 || ny >= col) continue;
-
-            // 목적지가 벽이고, 아직 뚫지 않았을 때,
-            if (board[nx][ny] && block) {
+            
+            if (board[nx][ny] && block) {  // -------------- 목적지가 벽이고, 아직 벽돌을 뚫지 않아봤을 때
                 dis[nx][ny][block - 1] = dis[temp.X][temp.Y][block] + 1;
-                q.push({ {nx,ny},block - 1 });
+                q.push({ {nx,ny},block - 1 }); // block은 -1 감소시킨 채로 queue에 넣는다
             }
-
-            // 목적지가 갈 수 있는 곳이고 아직 방문하지 않았을 때,
-            if (!board[nx][ny] && !dis[nx][ny][block]) {
+            
+            if (!board[nx][ny] && !dis[nx][ny][block]) { // -----목적지가 갈 수 있는 곳이고 아직 방문하지 않았을 때,
 
                 dis[nx][ny][block] = dis[temp.X][temp.Y][block] + 1;
                 q.push({ {nx,ny},block });
-
             }
         }
     }
